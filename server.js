568009ee78b8e023acf759d50961d6bb78305d88
Abhijeet raut <Abhijeetraut456@gmail.com>
2021-12-15 14:01:20 +0530

    deployed

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express'); //common js formate
 const connectDB = require('./config/db');
-const path = require("path")
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 
 // app.get('/', (req, res) =>
 //   res.json({ msg: 'Welcome to the contact keeper API' })
@@ -23,10 +21,14 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 //serve static assets in production
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('client/build'))
+  app.use(express.static('client/build'));
 
   //* means anything  not the about routes
-  app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'client','build','index.html')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
